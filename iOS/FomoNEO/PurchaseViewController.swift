7e68d285e53891176cd1d3feb6938e4c2efddb73
Wei Qiu <>
2018-07-29 20:59:48 -0400

    Init iOS project

---
+++ iOS/FomoNEO/PurchaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FomoNEO
+//
+//  Created by qiuwei on 2018-07-25.
+//  Copyright © 2018 qiuwei. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
